b4055ba8493879ab1f7795aa9edde8ae0be59054
Vos <stein.vos@capgemini.com>
2018-10-08 11:19:14 +0200

    [ADD]: add airplane form now works with backend

--- src/main/resources/static/js/airplaneCRUD.js
+++ src/main/resources/static/js/airplaneCRUD.js
@@ -1,6 +1,48 @@
 $(document).ready(function () {
     
+
+    // post new airplane
+    $("#newAirplaneForm").on('submit', function (e) {
+
+        // Post the data from the form
+        postAirplane();
+
+        // Reset form
+        $("#airplaneName").val("");
+        $("#airport").val("");
+    });
 });
 
 
 // create a new airplane and post it to the database
+function postAirplane() {
+    
+    // get data from html
+    let name = $("#airplaneName").val(); 
+    let airport = $("#airport").val();
+    
+    // create js object
+    let newAirplane = {
+        name: name,
+        airport: {"id": airport}
+    }
+
+    // create valid JSON
+    let validJsonAirplane = JSON.stringify(newAirplane); 
+
+    console.log(validJsonAirplane); 
+
+    // Post JSON to endpoint.
+    $.ajax({
+        url: "http://localhost:8080/api/airplane/",
+        type: "post",
+        data: validJsonAirplane,
+        contentType: "application/json",
+        success: function (result) {
+           
+        }
+    });
+    // close modal
+    $('#newSupplierBtn').click();
+
+}
